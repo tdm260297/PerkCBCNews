b34f168d16ee828e6a9d4cbeea47df9fcd1f9095
mehta <mehta@TARANG-PC>
2023-03-10 11:35:14 -0500

    Add project files.

---
+++ Default/CBCNews.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CBCNews.aspx.cs" Inherits="Default.CBCNews" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Top CBC News Stories</title>
+</head>
+<body>
+    <div>
+        <h1>Top CBC News Stories</h1>
+        <asp:Repeater ID="rptNews" runat="server">
+  <ItemTemplate>
+    <div class="news-item">
+      <img src='<%# Eval("ImageUrl") %>' alt='<%# Eval("Title") %>' />
+      <h2><%# Eval("Title") %></h2>
+      <p><%# Eval("Description") %></p>
+      <span><%# Eval("Author") %></span>
+    </div>
+  </ItemTemplate>
+</asp:Repeater>
+
+    </div>
+</body>
+</html>
---
+++ Default/CBCNews.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Xml;
+using System.Net;
+
+namespace Default
+{
+    public partial class CBCNews : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                string url = "http://www.cbc.ca/cmlink/rss-topstories";
+                XmlDocument doc = new XmlDocument();
+                doc.Load(url);
+                XmlNodeList nodes = doc.SelectNodes("//item");
+                DataTable dt = new DataTable();
+                dt.Columns.Add("title", typeof(string));
+                
+                dt.Columns.Add("ImageUrl", typeof(string));
+
+                dt.Columns.Add("description", typeof(string));
+                dt.Columns.Add("author", typeof(string));
+                foreach (XmlNode node in nodes)
+                {
+                    DataRow row = dt.NewRow();
+                    row["title"] = node.SelectSingleNode("title").InnerText;
+                    row["description"] = node.SelectSingleNode("description").InnerText;
+                    row["author"] = node.SelectSingleNode("author").InnerText;
+                    dt.Rows.Add(row);
+                }
+                rptNews.DataSource = dt;
+                rptNews.DataBind();
+            }
+        }
+
+    }
+}
